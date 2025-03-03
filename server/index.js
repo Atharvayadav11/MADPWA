056fe0027bcf9ed4dd9814b87e6ab7bc431a761e
Atharva Yadav <atharvayadav11@gmail.com>
2025-03-03 23:16:47 +0530

    bugs fixed

--- server/index.js
+++ server/index.js
@@ -9,7 +9,10 @@ import attemptRoutes from './routes/attempts.js';
 import path from 'path';
 
 dotenv.config();
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 const PORT = process.env.PORT || 5000;
 
